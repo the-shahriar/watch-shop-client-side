a1860bf7f9a9fd0f52dc61c086f9d96f7d366d31
Shahriar Islam <theshahriarislam@gmail.com>
2021-11-11 08:22:46 +0600

    register page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home/Home';
 import './App.css';
+import ExploreProducts from './Pages/ExploreProducts/ExploreProducts';
+import NotFound from './Pages/NotFound/NotFound';
+import Register from './Pages/Register/Register';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
      <Switch>
       <Route exact path="/">
         <Home></Home>
@@ -12,8 +15,17 @@ function App() {
       <Route path="/home">
         <Home></Home>
       </Route>
+      <Route path="/products">
+        <ExploreProducts></ExploreProducts>
+      </Route>
+      <Route path="/register">
+        <Register></Register>
+      </Route>
+      <Route path="*">
+        <NotFound></NotFound>
+      </Route>
      </Switch>
-    </BrowserRouter>
+    </Router>
   );
 }
 
---
+++ src/Pages/ExploreProducts/ExploreProducts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+import Footer from '../../Shared/Footer/Footer';
+
+const ExploreProducts = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+           <Footer></Footer> 
+        </div>
+    );
+};
+
+export default ExploreProducts;
